44d1deaeca333dcc902c47d1d11d0946487d3ecd
wubi-28 <wubitekola67@gmail.com>
2025-08-22 15:14:15 +0000

    Main

---
+++ notes.py
@@ -0,0 +1,21 @@
+# def is the keyword to define a function
+# whatType is the name of the function
+# userInput is the parameter of the function
+def whatType(userInput):
+    print(type)(userInput)
+    #print is a Python built-in function that prints to the console
+    # type is a Python built-in function that finds the datatype
+    # The program ignores all comments 
+    # userInput is a variable that the user enters
+        # The pound symbol is for one line comments
+        # The program ignores all comments 
+ 
+"""  multiple line comments
+
+ """
+ #Call the function with different user inputs
+whatType(3)
+whatType(3.0)
+whatType(True)
+whatType(Wubi)
+whatType('w')
